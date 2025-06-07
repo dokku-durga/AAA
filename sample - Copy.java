e5a6a10fefd5ad6b47c9721cc65cf87a452ff974
dokku-durga <kanakadurgadokku0@gmail.com>
2025-06-07 21:38:47 +0530

    successfully commit

---
+++ sample - Copy.java
@@ -0,0 +1,10 @@
+public class GFG {
+  
+    // Program begins with a call to main() method
+    public static void main(String args[])
+    {
+      
+          // Prints "Hello World" 
+        System.out.println("Hello World");
+    }
+}
